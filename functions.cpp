eae51e039df8b56a46895bcc3683d4e3c20bd83c
CarterCaleb83 <cartercaleb83@gmail.com>
2022-10-24 15:14:42 -0700

    Made three file structure with rough outline of ideas and some commenting. We will decide what kind of game we want to create at a later time but for now we will keep things vague but productive. Good luck everyone

---
+++ functions.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file functions.cpp
+ * @GroupName MalAarCal
+ * @GroupMembers Aaron Jarnes, Caleb Carter, Malik Robinson
+ * @brief Function list for game
+ * @date 10-24-2022
+ */
+
+//DO NOT FORGET TO CHECK README.md FREQUENTLY!!! Important updates and notes should be made here!
+
+#include "header.h"
+
+/*
+TODO: 
+refreshDisplay(); importGraphics(); More TBD.
+*/
+
