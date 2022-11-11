8825804b941734a638cbb21e16b0e9ec5e936e0a
Mazarus224 <102100760+Mazarus224@users.noreply.github.com>
2022-11-11 13:39:36 -0800

    Update Objects.h

--- Objects.h
+++ Objects.h
@@ -1,5 +1,12 @@
-#ifndef OBJECT_H
-#define OBJECT_H
+/**
+ * @file header.h
+ * @ingroup MalAarCal 
+ * @authors Aaron Jarnes, Caleb Carter, Malik Robinson
+ * @brief Header for game. Stores all important global information
+ * @date 10-24-2022
+ */
+#ifndef HEADER_H
+#define HEADER_H
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -7,27 +14,38 @@
 #include <unistd.h>
 #endif
 
+#include <iostream>
 #include <ctime>
+#include <string>
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
 
 using namespace sf;
 
 class Objects
 {
     public:
-   void makeFloor(int s);
-   void moveFloor(int s);
+    void makeFloor(int s);
+    void moveFloor(int s);
+    void makeBackground(int s);
+    void moveBackground();
+    void makePlayer();
+    void jump(int height, int falls, int rises, int plat, bool &nojump);
+
    
-   int floor1, floor2, floor3;
+    int floor1, floor2, floor3;
+    int backx, backx2;
 
    RectangleShape one, two, three;
+   RectangleShape background, background2;
+   RectangleShape obs, plat;
    CircleShape Player;
    Texture text1, text2, text3;
    Texture ptext;
    Texture ptext2;
+   Texture back;
 
-    void makePlayer();
-    void jump(int height, int falls, int rises, int plat, bool &nojump); 
+     
 
 
     int playerY = -125; 
@@ -35,5 +53,6 @@ class Objects
 
 
 };
+
 
 #endif
