91030f9b055b8db7b3b6b37cdd3a4ae5e07ffc77
Mazarus224 <102100760+Mazarus224@users.noreply.github.com>
2022-11-10 19:46:57 -0800

    Update Objects.h

--- Objects.h
+++ Objects.h
@@ -15,12 +15,25 @@ using namespace sf;
 class Objects
 {
     public:
-        void makeFloor(int s);
-        void moveFloor(int s);
-        
-        int floor1, floor2, floor3;
+   void makeFloor(int s);
+   void moveFloor(int s);
+   
+   int floor1, floor2, floor3;
 
-        RectangleShape one, two, three;
+   RectangleShape one, two, three;
+   CircleShape Player;
+   Texture text1, text2, text3;
+   Texture ptext;
+   Texture ptext2;
+
+    void makePlayer();
+    void jump(int height, int falls, int rises, int plat, bool &nojump); 
+
+
+    int playerY = -125; 
+    int playerX = -40;
+
+
 };
 
 #endif
